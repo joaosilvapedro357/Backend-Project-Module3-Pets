02975946f4b6404582608e928fcb2364026789c1
João Silva <joaosilvapedro357@gmail.com>
2023-11-29 17:17:10 +0000

    database connected

--- backend-upet-server/routes/auth.routes.js
+++ backend-upet-server/routes/auth.routes.js
@@ -58,15 +58,15 @@ router.post("/signup", (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name });
+      return User.create({ email, password: hashedPassword, name, phoneNumber, country, city, address, numberOfPets});
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
+      const { email, name, phoneNumber, country, city, address, numberOfPets, _id } = createdUser;
 
       // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
+      const user = { email, name, phoneNumber, country, city, address, numberOfPets, _id };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
@@ -98,10 +98,10 @@ router.post("/login", (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, name } = foundUser;
+        const { _id, email, name, phoneNumber, country, city, address, numberOfPets} = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, name };
+        const payload = { _id, email, name, phoneNumber, country, city, address, numberOfPets };
 
         // Create a JSON Web Token and sign it
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
