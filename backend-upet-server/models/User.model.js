165d7a149141eb34424d83bf120e1cb7a2385657
João Silva <joaosilvapedro357@gmail.com>
2023-12-05 11:32:51 +0000

    minor updates in models

--- backend-upet-server/models/User.model.js
+++ backend-upet-server/models/User.model.js
@@ -6,11 +6,11 @@ const userSchema = new Schema(
     email: { type: String, required: [true, "Email is required."], unique: true, 
     lowercase: true, trim: true },
     password: { type: String, required: [true, "Password is required."] },
-    name: { type: String, required: [true, "Name is required."] },
-    phoneNumber: { type: String, required: true, unique: true },
-    country: { type: String, required: true },
-    city: { type: String, required: true },
-    address: { type: String, required: true },
+    name: { type: String, required: false},
+    phoneNumber: { type: String},
+    country: { type: String, required: false },
+    city: { type: String, required: false },
+    address: { type: String, required: false },
     numberOfPets: { type: Number }
     
   }
